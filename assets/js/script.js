341d49d42806ebab198d7b3470d15e4ba9484a47
vikram <vikrammudhiraj85@gmail.com>
2021-01-11 19:49:22 +0530

    modify digital twin link

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,3 +130,20 @@ function handleTouchMove(evt) {
     xDown = null;
     yDown = null;                                             
 };
+
+function searchFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementsByClassName("searchInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementsById("myUL");
+  li = ul.getElementsById("myLI");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
